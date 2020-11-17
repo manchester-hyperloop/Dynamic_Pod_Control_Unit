8db57d84548e47f02be87f4421e3ee65131bfa2e
Mikolaj Lenczewski <mikolaj.lenczewski308@gmail.com>
2020-11-17 21:39:03 +0000

    Renamed __time_t, added __pid_value_t

--- include/Core/Pid.hpp
+++ include/Core/Pid.hpp
@@ -1,22 +1,24 @@
 #ifndef CORE_PID_HPP
 #define CORE_PID_HPP
 
+// allows us to quickly switch the precision of the pid
+typedef double __pid_value_t;
 
-// `__time_t` is shorter than `unsigned long`, and we dont use `uxxx_t` :(
-typedef unsigned long __time_t;
+// allows us to quickly switch the backing time container type
+typedef unsigned long __pid_time_t;
 
 namespace Core {
     /// Proportional-Integral-Derivative controller, used to converge to a set value over time.
     class Pid {
     private:
-        double m_setpoint; // target value
-        double m_kp, m_ki, m_kd; // tuning parameters
-        double m_error_sum; // used to calculate the integral error
-        double m_last_error; // used to calculate the derivative error
-        __time_t m_last_measured_us; // the last time a measurement was made
+        __pid_value_t m_setpoint; // target value
+        __pid_value_t m_kp, m_ki, m_kd; // tuning parameters
+        __pid_value_t m_error_sum; // used to calculate the integral error
+        __pid_value_t m_last_error; // used to calculate the derivative error
+        __pid_time_t m_last_measured_us; // the last time a measurement was made
 
         // scaling factor to leave pid params in 1000s
-        static constexpr double __pid_param_scale = 0.001;
+        static constexpr __pid_value_t PARAM_SCALE = 0.001;
 
     public:
         /// Initialises a new instance of the Pid class.
@@ -24,7 +26,7 @@ namespace Core {
         /// @param kp The proportional error tuning parameter. How hard we work to correct error
         /// @param ki The integral error tuning parameter. How much we counteract drift in the measured value
         /// @param kd The derivative error tuning parameter. How much we dampen overcorrection
-        Pid(const double setpoint, const double kp, const double ki, const double kd)
+        Pid(const __pid_value_t setpoint, const __pid_value_t kp, const __pid_value_t ki, const __pid_value_t kd)
             : m_setpoint(setpoint), m_kp(kp), m_ki(ki), m_kd(kd)
         {
             m_error_sum = 0;
@@ -37,24 +39,24 @@ namespace Core {
         /// @param current_time_us The current time. Must be > 0 and must only increase
         /// @returns The target value necessary to reach the setpoint
         /// @remark Adapted from https://en.wikipedia.org/wiki/PID_controller#Controller_theory
-        double next(const double source, const __time_t current_time_us)
+        double next(const __pid_value_t source, const __pid_time_t current_time_us)
         {
             // calculate a dt between measurements
-            const double dt = (double)(current_time_us - m_last_measured_us) / 1000000.0;
+            const __pid_value_t dt = (__pid_value_t)(current_time_us - m_last_measured_us) / 1000000.0;
 
             // calculate new error between source value and target value
-            const double error = m_setpoint - source;
+            const __pid_value_t error = m_setpoint - source;
 
             // update integral error
             m_error_sum += error * dt;
 
             // get the gradient between the last 2 error values, for the derivative error
-            const double gradient = (error - m_last_error) / dt;
+            const __pid_value_t gradient = (error - m_last_error) / dt;
 
             // assigns temporary variables
-            const double proportional = (m_kp * __pid_param_scale) * error;
-            const double integral = (m_ki * __pid_param_scale) * m_error_sum;
-            const double derivative = (m_kd * __pid_param_scale) * gradient;
+            const __pid_value_t proportional = (m_kp * PARAM_SCALE) * error;
+            const __pid_value_t integral = (m_ki * PARAM_SCALE) * m_error_sum;
+            const __pid_value_t derivative = (m_kd * PARAM_SCALE) * gradient;
 
             // update last measurement time and last measured error
             m_last_error = error;
